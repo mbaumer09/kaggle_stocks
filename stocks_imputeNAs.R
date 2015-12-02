a431f854e3ead48f3ed416e8452e2427f0453b90
mbaumer09 <mbaumer09@gmail.com>
2015-12-02 09:23:29 -0800

    Commit 2
    Update script to impute NA values in features

--- stocks_imputeNAs.R
+++ stocks_imputeNAs.R
@@ -15,6 +15,7 @@ setwd("C:/Users/USER/Dropbox/Side Project/Machine Learning/Kaggle/Winton Stock M
 library(ggplot2)
 library(dplyr)
 library(reshape2)
+library(caret)
 
 # This loads stock data if the current df labeled 'train' is not the stock data or does not exist
 # and does not load it if it's already loaded up, saves a bit of computation
@@ -33,12 +34,58 @@ if(current.train != "stocks"){
 # The features with no NA values are 5 and 7
 noNAs <- c("Feature_5", "Feature_7")
 feature.names <- names(train)[2:26]
-colIndex <- 2:26
+colIndex <- 1:25
 feature.names.NAs <- colIndex[-which(feature.names %in% noNAs)]
 df.features <- train[,2:26]
 
 # For each feature except 5 and 7, train a model based on the non-NA values and use predicted values
 # as imputed values to replace the NAs
-for(col in feature.names.NAs[1]){
-      df.temp <- df.features[-is.na(df.features[,col]),0]
+init <- 0
+i <- 1
+for(col in feature.names.NAs[1:2]){
+      df.temp <- df.features[!is.na(df.features[,col]),]
+      predictors <- df.temp[,-col]
+      if(length(unique(df.temp[,col]))<50){
+            outcome <- factor(df.temp[,col])
+            preproc <- preProcess(x = predictors,
+                                  method = c("bagImpute"),
+                                  na.remove = TRUE)
+            predictors.preproc <- predict(preproc, predictors)
+            print(paste("Done with feature ", i, " preprocess", sep=""))
+            model <- train(x = predictors.preproc,
+                           y = outcome,
+                           method = "rf")
+            pred.acc <- predict(model, predictors.preproc)
+            acc <- sum(pred.acc == outcome)/length(pred.acc)
+            pred.preproc.full <- predict(preproc, df.features[,2:25])
+            predicted <- predict(model, pred.preproc.full)
+      }
+      else{
+            outcome <- df.temp[,col]
+            preproc <- preProcess(x = predictors,
+                                  method = c("bagImpute"),
+                                  na.remove = TRUE)
+            predictors.preproc <- predict(preproc, predictors)
+            print(paste("Done with feature ", i, " preprocess", sep=""))
+            model <- train(x = predictors.preproc,
+                           y = outcome,
+                           method = "lasso")
+            pred.acc <- predict(model, predictors.preproc)
+            acc <- sum(pred.acc == outcome)/length(pred.acc)
+            pred.preproc.full <- predict(preproc, df.features[,-col])
+            predicted <- predict(model, pred.preproc.full)
+      }
+     
+      
+      if(init == 0){
+            train.impute = data.frame(predicted)
+            init <- 1
+      }
+      else{
+            train.impute <- cbind(train.impute, predicted)
+      }
+      colname.temp <- paste("F", i, "_imputed")
+      names(train.impute)[i] <- colname.temp
+      print(paste("Done with feature ", i, " model", sep=""))
+      i <- i+1
 }
